b93df34f9ceb9055563cdc48b58eb1c620830d1d
hariom nathani <hariomnathani551@gmail.com>
2024-02-21 08:34:19 +0530

    middleware multer fileupload

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,16 +3,16 @@ class ApiError extends Error{
         statusCode,
         message= "something went wrong",
         errors =[],
-        statck = ""
+        stack = ""
     ){
         super(message)
         this.statusCode = statusCode
         this.data =null
         this.message=message
-        this .success = false;
+        this.success = false;
         this.errors = errors
 
-        if(statck){
+        if(stack){
             this.stack = statck
         }else{
             Error.captureStackTrace(this, this.constructor)
