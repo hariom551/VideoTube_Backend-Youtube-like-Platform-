909ccb46216f9c93c9f52dcaba218b711d595d77
hariom nathani <hariomnathani551@gmail.com>
2024-02-23 22:45:51 +0530

    update login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,22 +16,22 @@ const registerUser = asyncHandler(async(req, res) =>{
     // remove password and refresh token field from response
     // check for user creation
     // return res
-
+try{
     const {fullName, email, username, password } =req.body   // req.body express ne dia h  
-    console.log("email: ", email);
+    // console.log("email: ", email);
     
     // if (fullName === ""){
     //     throw new ApiError(400, "fullname is required")
     // }
 
-// check all together
+// check all together  // Validate input fields
     if (
-        [fullName, email, username, password].some((field) => field?.trim() === "")
+        [fullName, email, username, password].some((field) => !field || field?.trim() === "")
     ) {
         throw new ApiError(400, "All fields are required")
  } 
     
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, {email}]
     })
     
@@ -39,15 +39,22 @@ const registerUser = asyncHandler(async(req, res) =>{
         throw new ApiError (409, "User with email or username already exists")
     }
 
-
+    // console.log(req.files)
+    // console.log(req.body)
     // req.body me sare data aata but also hm lof route k andr ek middle ware addkiye h toh middleware also give some access its add some access in req field like req.files 
     
     const avatarLocalPath = req.files?.avatar[0]?.path; 
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
     }
+    
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
+    
 
     
     // ? use for optionaly check for access available or not
@@ -64,11 +71,10 @@ const registerUser = asyncHandler(async(req, res) =>{
         fullName,
         avatar:avatar.url,
         coverImage: coverImage?.url || "",
-        // becoz cover image not requires
         email,
         password,
         username: username.toLowerCase()
-    })
+    });
 
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
@@ -80,12 +86,36 @@ const registerUser = asyncHandler(async(req, res) =>{
     
     return res.status(201).json(
         new ApiResponse(200, createdUser, "User registered successfully")
-    )
-   
+    );
+}catch(error){
+    console.error(error);
+    res.status(error.statusCode || 500).json(new ApiResponse(error.statusCode || 500, null, error.message || "Internal Server Error"));
+
+}
     // res.status(200).json({
     //     message: "okay"
     // })
 })
+
+const loginUser = asyncHandler(async (req,res)=>{
+    //
+    // req body -> data lao
+    // username or email login
+    // find the user
+    // validate the password check
+    // access and refresh token generate
+    // send in cookie
+    // response successfully login 
+
+    const {email, username, password} = req.body 
+
+    if(!usernmae || !email){
+        throw new ApiError(400, "username or password is requires")
+    }
+
+    
+})
+
 
 
 
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,9 +1,10 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb){
-        cb(null, ".public/temp")
-    },
+    // destination: function (req, file, cb){
+    //     cb(null, ".public/temp")
+    // },
+    destination: 'D:/Webdec/NODE/YoutubeBackend/.public/temp/',
     filename: function (req, file, cb){
         // const uniqueSuffix = Date.now() + '-' +Math.round(Math.random() * 1E9)
         // cb(null, file.fieldname + '-' + uniqueSuffix)
